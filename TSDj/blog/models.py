e5cc6066f104d42cf2bfb5cb9148782c49e27e09
Maxim3288 <maxim3288@.com>
2025-12-04 01:12:31 +0300

    Add forms (Create, Deltail, Update, Delete) for Pos App

--- TSDj/blog/models.py
+++ TSDj/blog/models.py
@@ -7,9 +7,14 @@ class Post(models.Model):
         "auth.User", on_delete=models.CASCADE,
     )
     body = models.TextField()
-def __str__(self):
-    return self.title
-def get_absolute_url(self):
-    return reverse("post_detail", kwargs={"pk":self.pk})
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk":self.pk})
+
+# def get_absolute_url(self):
+#     return reverse("post_new", kwargs={"pk":self.pk})
             
 # Create your models here.
